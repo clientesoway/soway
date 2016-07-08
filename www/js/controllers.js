2c64053e57bef0bc8b98a198e6407bb5d0a5c825
Tiago Machado <tiago.machado@supero.com.br>
2016-07-08 07:59:32 -0300

    commit 3

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('pageCtrl', function($scope) {
+
+})
+   
+.controller('recuperarSenhaCtrl', function($scope) {
+
+})
+ 
