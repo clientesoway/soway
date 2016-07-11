64038582f9c847e4b3a97c4880f414ca5386c70c
Tiago Machado <tiago.machado@supero.com.br>
2016-07-11 14:44:14 -0300

    commit 4

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,6 +8,54 @@ angular.module('app.controllers', [])
 
 })
    
+.controller('sobreAFAMBRASCtrl', function($scope) {
+
+})
+   
+.controller('certificaOHalalCtrl', function($scope) {
+
+})
+   
+.controller('sobreACibalHalalCtrl', function($scope) {
+
+})
+   
+.controller('verificarCertificadoCtrl', function($scope) {
+
+})
+   
+.controller('verificarCertificadoFrigorFicoCtrl', function($scope) {
+
+})
+   
+.controller('governosCtrl', function($scope) {
+
+})
+   
+.controller('governosCertificadoCtrl', function($scope) {
+
+})
+   
+.controller('governosControleDeQualidadeCtrl', function($scope) {
+
+})
+   
+.controller('governosFrigorFicoCtrl', function($scope) {
+
+})
+   
+.controller('controleDeQualidadeCtrl', function($scope) {
+
+})
+   
+.controller('controleDeQualidadeInvoiceCtrl', function($scope) {
+
+})
+   
+.controller('entrarEmContatoCtrl', function($scope) {
+
+})
+   
 .controller('recuperarSenhaCtrl', function($scope) {
 
 })
--- www/js/routes.js
+++ www/js/routes.js
@@ -22,6 +22,78 @@ angular.module('app.routes', [])
     controller: 'homeCtrl'
   })
 
+  .state('sobreAFAMBRAS', {
+    url: '/page7',
+    templateUrl: 'templates/sobreAFAMBRAS.html',
+    controller: 'sobreAFAMBRASCtrl'
+  })
+
+  .state('certificaOHalal', {
+    url: '/page9',
+    templateUrl: 'templates/certificaOHalal.html',
+    controller: 'certificaOHalalCtrl'
+  })
+
+  .state('sobreACibalHalal', {
+    url: '/page8',
+    templateUrl: 'templates/sobreACibalHalal.html',
+    controller: 'sobreACibalHalalCtrl'
+  })
+
+  .state('verificarCertificado', {
+    url: '/page10',
+    templateUrl: 'templates/verificarCertificado.html',
+    controller: 'verificarCertificadoCtrl'
+  })
+
+  .state('verificarCertificadoFrigorFico', {
+    url: '/page11',
+    templateUrl: 'templates/verificarCertificadoFrigorFico.html',
+    controller: 'verificarCertificadoFrigorFicoCtrl'
+  })
+
+  .state('governos', {
+    url: '/page12',
+    templateUrl: 'templates/governos.html',
+    controller: 'governosCtrl'
+  })
+
+  .state('governosCertificado', {
+    url: '/page14',
+    templateUrl: 'templates/governosCertificado.html',
+    controller: 'governosCertificadoCtrl'
+  })
+
+  .state('governosControleDeQualidade', {
+    url: '/page15',
+    templateUrl: 'templates/governosControleDeQualidade.html',
+    controller: 'governosControleDeQualidadeCtrl'
+  })
+
+  .state('governosFrigorFico', {
+    url: '/page16',
+    templateUrl: 'templates/governosFrigorFico.html',
+    controller: 'governosFrigorFicoCtrl'
+  })
+
+  .state('controleDeQualidade', {
+    url: '/page17',
+    templateUrl: 'templates/controleDeQualidade.html',
+    controller: 'controleDeQualidadeCtrl'
+  })
+
+  .state('controleDeQualidadeInvoice', {
+    url: '/page18',
+    templateUrl: 'templates/controleDeQualidadeInvoice.html',
+    controller: 'controleDeQualidadeInvoiceCtrl'
+  })
+
+  .state('entrarEmContato', {
+    url: '/page20',
+    templateUrl: 'templates/entrarEmContato.html',
+    controller: 'entrarEmContatoCtrl'
+  })
+
   .state('recuperarSenha', {
     url: '/page4',
     templateUrl: 'templates/recuperarSenha.html',
